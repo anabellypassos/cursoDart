07318281b7e0020ee6fbae360a60a78a5d49a673
Anabelly <anabellybel123@gmail.com>
2024-08-12 12:13:20 -0300

    variaveis 2

---
+++ fundamentos/variaveis_2.dart
@@ -0,0 +1,36 @@
+void main() {
+  var n1 = 3;
+  var n2 = 4.56;
+  var texto = "O valor da sona é : ";
+
+  print(texto + (n1 + n2).toString());
+
+ // print(n1.runtimeType);
+ // print(n2 is int);
+}
+
+
+//Inferehncia
+
+
+//Quando o dart entende que o tipo de variavel  é:
+
+// var n1 = 3;( Inteiro)
+
+ //var n2 = 4.56;( double )
+ // var texto = "O valor da sona é : " (String);
+
+
+
+
+ //Concatenação :
+
+ // print(texto + (n1 + n2).toString());
+ //Junto o texto mais o valor da soma
+
+
+//Como saber o tipo da variavel:
+
+// print(n1.runtimeType); (Diz o tipo da variavel)
+
+//print(n2 is int); (diz se é verdadeiro ou falso)
