819bbfb0809f5312ba262023eb405f0628fe64a2
Anabelly <anabellybel123@gmail.com>
2024-08-12 11:30:49 -0300

    variaveis 1

--- fundamentos/primeiro.dart
+++ fundamentos/primeiro.dart
@@ -1,4 +1,7 @@
 main(){
   print('Olá Dart!');
    print("com aspas duplas funciona também!");
+   
 }
+
+
---
+++ fundamentos/variaveis_1.dart
@@ -0,0 +1,27 @@
+void main() {
+  int a = 2;
+  double b = 3.5456;
+  print(a);
+  print(b);
+  b = 3.1415;
+
+
+  print(a * b);
+}
+
+
+//variaveis: 
+
+//Definida com o  tipo , nome e valor.
+
+//int (tipo)
+//a (nome)
+// = (recebe)
+// 2 valor 
+
+
+
+//Tipo double
+
+//Recebe números decimais
+// double b = 3.1314
