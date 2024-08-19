6f139adf773312d4e230597550bf0bca5925916e
anabellypassos <anabellybel123@gmail.com>
2024-08-19 15:27:11 -0300

    Operadores

---
+++ Operadores_1.dart
@@ -0,0 +1,24 @@
+void main() {
+//Aritmétisos (Operadores binários / infix)
+
+  int a = 7;
+  int b = 3;
+
+  int resultado = a + b;
+
+  print(resultado);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+
+
+  //Operadoees Lógicos
+  bool ehFragil = true;
+  bool ehCaro = false;
+
+  print(ehFragil && ehCaro); // E
+  print(ehFragil || ehCaro); // OU
+  print(ehFragil ^ ehCaro); //Ou Exclusivo
+  print(!ehFragil); // Não  //Únario / Prefixado
+}
