40f81ba9113412aad5ee344b2bfa9cc69bdf5519
anabellypassos <anabellybel123@gmail.com>
2024-08-19 20:58:03 -0300

    Operadores 2

---
+++ Operadores_2.dart
@@ -0,0 +1,22 @@
+void main() {
+  //Operadores de Atribuição (binário/infix)
+
+  double a = 2;
+  a = a + 3;
+  a += 3;
+  a -= 3;
+  a *= 3;
+  a /= 5;
+
+  print(a);
+
+  //Operadores Relacionais (binário/ infix) -> O resultado sempre é BOOL
+
+  print(3 > 2);
+  print(3 >= 2); 
+  print(3 < 4);
+  print(3 <=  3);
+  print(3 !=  3);
+  print(3 ==  3);
+  
+}
