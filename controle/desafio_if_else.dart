c8150e624d4822d8879c7f7673751bf2465a4749
anabellypassos <anabellybel123@gmail.com>
2024-08-25 14:14:22 -0300

    desafio

---
+++ controle/desafio_if_else.dart
@@ -0,0 +1,9 @@
+void main() {
+  var nota = 2.3;
+
+  if (nota >= 9.0) {
+    print('Parabéns ! VC foi brilhante!!!');
+  }
+}
+
+//não se termina estrutura de controle com ponto e virgula, com exeção do DO/WHILE
